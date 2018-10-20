6e015095ded42ce29d909f495ceba6095780ac19
SuiFengFX <930782689@qq.com>
2018-10-20 09:34:28 +0800

    commit src file

---
+++ app/src/main/java/com/huanxin/sffx/gitleans/MainActivity.java
@@ -0,0 +1,19 @@
+package com.huanxin.sffx.gitleans;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.security.Security;
+
+import javax.crypto.KeyGenerator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        KeyGenerator
+        Security
+    }
+}
